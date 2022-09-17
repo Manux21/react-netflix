b3314fb353310cd508e267c8ab415ec4532ebcdc
Manux21 <Manux21@yandex.ru>
2022-09-17 20:21:21 +0300

    updated logo and button color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import netflixLogo from '../img/Logonetflix.png'
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-      <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
+      <div>
+        <img className=' cursor-pointer' src={netflixLogo} alt="netflixLogo"/>
+      </div>
+      {/*<h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>*/}
       <div>
         <button className='text-white pr-4'>Sign in</button>
-        <button className='bg-red-500 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
+        <button className='bg-red-700 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
       </div>
     </div>
   );
