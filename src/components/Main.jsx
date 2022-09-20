689d06464e5da5590327302a1ebbabe4b0e9b768
Manux21 <Manux21@yandex.ru>
2022-09-21 00:17:11 +0300

    preparing to use firebase

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -43,7 +43,7 @@ const Main = () => {
               Released: {movie?.release_date}
             </p>
             <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>
-              {truncateString(movie?.overview, 150)}
+              {truncateString(movie?.overview, 250)}
             </p>
           </div>
         </div>
