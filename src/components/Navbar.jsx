6bcb6c25ca90de241717e79044ba82d2664262dd
Manux21 <Manux21@yandex.ru>
2022-09-21 22:53:43 +0300

    added sign in and sign up pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import netflixLogo from '../img/Logonetflix.png'
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-      <div>
-        <img className=' cursor-pointer' src={netflixLogo} alt="netflixLogo"/>
-      </div>
+      <Link to='/'>
+        <div>
+          <img className=' cursor-pointer' src={netflixLogo} alt="netflixLogo"/>
+        </div>
+      </Link>
+
       {/*<h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>*/}
       <div>
-        <button className='text-white pr-4'>Sign in</button>
-        <button className='bg-red-700 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
+        <Link to='/login'>
+          <button className='text-white pr-4'>Sign in</button>
+        </Link>
+
+        <Link to='/signup'>
+          <button className='bg-red-700 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
+        </Link>
+
       </div>
     </div>
   );
